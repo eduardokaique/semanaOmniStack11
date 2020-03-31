a09de0796b55cd5200a6a32fe41788e47178f04f
Eduardo Kaique Freitas <eduardokaique.dev@gmail.com>
2020-03-31 02:42:06 -0300

    build de projeto pata publicar no netlify.

---
+++ frontend/build/precache-manifest.fa6db670c77bdba6a51d5cc02bc54b63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585181210e7961c89c10da319bdddab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfde7b6ca3bb48ba0a7d",
+    "url": "/static/css/main.16456cc0.chunk.css"
+  },
+  {
+    "revision": "19d7d3b4baacab3db35d",
+    "url": "/static/js/2.2b93c1ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b93c1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfde7b6ca3bb48ba0a7d",
+    "url": "/static/js/main.9928f8ad.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/static/media/logo.62e865e1.svg"
+  }
+]);
