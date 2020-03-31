bfef0eaeeee710a5fda2a89876fe5420690ba0b6
Eduardo Kaique Freitas <eduardokaique.dev@gmail.com>
2020-03-31 02:07:09 -0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,6 @@ app.use(cors(
 app.use(express.json());
 app.use(routes);
 
-var porta = process.env.PORT || 8080;
-app.listen(porta);
+const port  = process.env.PORT || 3000;
+app.listen(port);
 // app.listen(3333);
